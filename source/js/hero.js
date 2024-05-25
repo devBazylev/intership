b230c122c975e085ff28bd9df01dc5a126a368b3
jar <dev.bazylev@gmail.com>
2024-05-25 18:38:00 +0300

    adds multiple paginations on slider

--- source/js/hero.js
+++ source/js/hero.js
@@ -1,12 +1,10 @@
 import Swiper from 'swiper';
 import { Pagination } from 'swiper/modules';
-// import { resetClassArray } from './util';
+import { addClass, addClassArray, resetClassArray, addListener } from './util';
 // import { addClass, removeClass, toggleClass, addListener, addListenerArray, removeListener, removeListenerArray, isTargetClick, isKeydown } from './util';
 
-const paginations = document.querySelectorAll('.hero__pagination');
-let pagId = 2;
-
-const paginationActive = paginations[pagId];
+const hero = document.querySelector('.hero');
+const paginations = Array.from(hero.querySelectorAll('.hero__pagination'));
 
 new Swiper('.hero', {
 // const swiper = new Swiper('.hero', {
@@ -15,14 +13,14 @@ new Swiper('.hero', {
   watchSlidesProgress: true,
   slideActiveClass: 'hero__slide--active',
   autoHeight: true,
-  pagination: {
-    el: paginationActive,
-    bulletActiveClass: 'hero__bullet--active',
-    bulletClass: 'hero__bullet',
-    type: 'bullets',
-    bulletElement: 'div',
-    clickable: true,
-  },
+  // pagination: {
+  //   el: paginationActive,
+  //   bulletActiveClass: 'hero__bullet--active',
+  //   bulletClass: 'hero__bullet',
+  //   type: 'bullets',
+  //   bulletElement: 'div',
+  //   clickable: true,
+  // },
   breakpoints: {
     320: {
       spaceBetween: 0,
@@ -36,16 +34,27 @@ new Swiper('.hero', {
   },
   on: {
     init: function () {
-      const bullets = document.querySelectorAll('.hero__bullet');
-      bullets.forEach((bullet) => {
-        bullet.setAttribute('aria-label', 'Переключите слайд');
-        bullet.setAttribute('tabindex', '0');
+      paginations.forEach((elem) => {
+        const bullets = elem.querySelectorAll('.hero__bullet');
+        addClass(bullets[0], 'hero__bullet--active');
+        bullets.forEach((bullet) => {
+          bullet.setAttribute('aria-label', 'Переключите слайд');
+          bullet.setAttribute('tabindex', '0');
+        });
+        for (let i = 0; i < bullets.length; i++) {
+          bullets[i].dataset.id = i;
+          const changeBullet = () => {
+            const bulletsAll = hero.querySelectorAll('.hero__bullet');
+            const xxx = hero.querySelectorAll('[data-id="' + i + '"]');
+            // console.log(xxx);
+            resetClassArray(bulletsAll, 'hero__bullet--active');
+            this.slideTo(i);
+            addClassArray(xxx, 'hero__bullet--active');
+          };
+          addListener(bullets[i], 'click', changeBullet);
+        }
       });
-      // this.slideTo(1);
     },
-    // slideChange: function () {
-    //   pagId = this.clickedIndex;
-    //   console.log(this.clickedIndex);
-    // },
   },
 });
+// accordId = +this.getAttribute('data-id');
