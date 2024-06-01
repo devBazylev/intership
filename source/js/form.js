366809c7d003f43a5717e37a9ee9b270bf8d1a0e
jar <dev.bazylev@gmail.com>
2024-06-01 19:31:55 +0300

    adds form js

--- source/js/form.js
+++ source/js/form.js
@@ -1,1 +1,20 @@
-// import { addClass, removeClass, addListener, addListenerArray, removeListener, isTargetClick, isKeydown } from './util';
+import { addClass, addListener, removeListener } from './util';
+
+const form = document.querySelector('.form__template');
+const submitButton = form.querySelector('.form__submit');
+
+const onSubmit = async (evt) => {
+  form.submit();
+  evt.preventDefault();
+  form.reset();
+};
+
+const onClick = () => {
+  if (!form.classList.contains('form__template--validation')) {
+    addClass(form, 'form__template--validation');
+  }
+  removeListener(submitButton, 'click', onClick);
+  addListener(form, 'submit', onSubmit);
+};
+
+addListener(submitButton, 'click', onClick);
