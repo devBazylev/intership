ecf38940e050a7f37be4a70f46770079ce6e4561
jar <dev.bazylev@gmail.com>
2024-06-08 21:53:59 +0300

    adds modal-block custom select layout and js

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,4 +1,4 @@
-import { addClass, removeClass, addListener, addListenerArray, removeListener, isTargetClick, isKeydown } from './util';
+import { addClass, removeClass, addListener, addListenerArray, removeListener, removeListenerArray, isTargetClick, isKeydown, toggleClass } from './util';
 
 const body = document.querySelector('.page__body');
 const overlay = body.querySelector('.page__overlay');
@@ -9,6 +9,11 @@ const submitButton = modal.querySelector('.modal__submit');
 const cancelButton = modal.querySelector('.modal__cancel');
 const checkboxInvisible = modal.querySelector('.modal__checkbox');
 const checkboxBackup = modal.querySelector('.modal__check');
+const select = modal.querySelector('.modal__select');
+const inputHidden = select.querySelector('.modal__hidden');
+const city = select.querySelector('.modal__city');
+const cities = select.querySelectorAll('.modal__item');
+const label = modal.querySelector('.modal__label--city');
 
 const clickCheckbox = (evt) => {
   if (isKeydown(evt, ' ')) {
@@ -49,6 +54,47 @@ const onCheckbox = (evt) => {
   clickCheckbox(evt);
 };
 
+const onSelectMissClick = (evt) => {
+  if (!isTargetClick(evt, '.modal__select') && !isTargetClick(evt, '.modal__label--city')) {
+    removeClass(select, 'modal__select--opened');
+    removeListener(document, 'click', onSelectMissClick);
+  }
+};
+
+const toggleSelect = () => {
+  toggleClass(select, 'modal__select--opened');
+  addListener(document, 'click', onSelectMissClick);
+};
+
+const onSelectClick = () => {
+  toggleSelect();
+};
+
+const onSelectKeydown = (evt) => {
+  if (isKeydown(evt, 'Enter')) {
+    toggleSelect();
+  }
+};
+
+function onCitiesClick () {
+  inputHidden.value = this.textContent;
+  city.textContent = this.textContent;
+  removeClass(select, 'modal__select--opened');
+}
+
+function onCitiesKeydown (evt) {
+  if (isKeydown(evt, 'Enter')) {
+    inputHidden.value = this.textContent;
+    city.textContent = this.textContent;
+    removeClass(select, 'modal__select--opened');
+  }
+}
+
+const onLabel = () => {
+  toggleSelect();
+  city.focus();
+};
+
 const onHeroButton = () => {
   addClass(body, 'page__body--no-scroll');
   addClass(modal, 'hero__form--opened');
@@ -59,6 +105,11 @@ const onHeroButton = () => {
   addListener(document, 'keydown', onDocument);
   addListener(document, 'click', onMissClick);
   addListener(checkboxBackup, 'keydown', onCheckbox);
+  addListener(label, 'click', onLabel);
+  addListener(city, 'click', onSelectClick);
+  addListener(city, 'keydown', onSelectKeydown);
+  addListenerArray(cities, 'click', onCitiesClick);
+  addListenerArray(cities, 'keydown', onCitiesKeydown);
 };
 
 function closeModal () {
@@ -71,6 +122,12 @@ function closeModal () {
   removeListener(document, 'keydown', onDocument);
   removeListener(document, 'click', onMissClick);
   removeListener(checkboxBackup, 'keydown', onCheckbox);
+  removeListener(label, 'click', onLabel);
+  removeListener(city, 'click', onSelectClick);
+  removeListener(city, 'keydown', onSelectKeydown);
+  removeListenerArray(cities, 'click', onCitiesClick);
+  removeListenerArray(cities, 'keydown', onCitiesKeydown);
+  removeClass(select, 'modal__select--opened');
 }
 
 addListenerArray(heroButtons, 'click', onHeroButton);
