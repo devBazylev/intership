c45ab279a79b36c9ea86cbb01847c6a6ebfa6025
jar <dev.bazylev@gmail.com>
2024-06-11 23:23:45 +0300

    news js dev

--- source/js/news.js
+++ source/js/news.js
@@ -2,6 +2,9 @@ import Swiper from 'swiper';
 import { Navigation, Pagination, Scrollbar, Manipulation, Grid } from 'swiper/modules';
 import { cloneSlides } from './util';
 
+const mob = window.matchMedia('(min-width: 0px) and (max-width: 767px)');
+const tab = window.matchMedia('(min-width: 768px) and (max-width: 1439px)');
+const desk = window.matchMedia('(min-width: 1440px)');
 const news = document.querySelector('.news');
 const slider = news.querySelector('.news__slider');
 const slides = news.querySelectorAll('.news__slide');
@@ -10,6 +13,33 @@ const clones = [];
 
 cloneSlides(slider, slides, clones);
 cloneSlides(slider, slides, clones);
+
+const resizeSlides = () => {
+  const slidesWithClones = news.querySelectorAll('.news__slide');
+  const slideActive = news.querySelector('.news__slide--active');
+  if (mob.matches) {
+    slidesWithClones.forEach((slide) => {
+      slide.style.width = '290px';
+      slide.style.height = '240px';
+    });
+    slideActive.style.width = '290px';
+    slideActive.style.height = '330px';
+  }
+  if (tab.matches) {
+    slidesWithClones.forEach((slide) => {
+      slide.style.width = '324px';
+      slide.style.height = '350px';
+    });
+  }
+  if (desk.matches) {
+    slidesWithClones.forEach((slide) => {
+      slide.style.width = '286px';
+      slide.style.height = '400px';
+    });
+    slideActive.style.width = '604px';
+    slideActive.style.height = '400px';
+  }
+};
 
 const swiper = new Swiper('.news__container', {
   modules: [Navigation, Pagination, Scrollbar, Manipulation, Grid],
@@ -21,8 +51,6 @@ const swiper = new Swiper('.news__container', {
   observeSlideChildren: true,
   slideFullyVisibleClass: 'news__slide--full',
   slideActiveClass: 'news__slide--active',
-  // slideNextClass: 'news__slide--next',
-  slideToClickedSlide: true,
   loopAddBlankSlides: false,
   loopAdditionalSlides: 0,
   resizeObserver: true,
@@ -44,13 +72,12 @@ const swiper = new Swiper('.news__container', {
       return `<button class="${className}" type="button" tabindex="0" aria-label="Переключите группу слайдов.">${index + 1}</button>`;
     }
   },
-
   breakpoints: {
     320: {
       width: 290,
       slidesPerView: 1,
       slidesPerGroup: 1,
-      spaceBetween: 15,
+      spaceBetween: 20,
       grid: {
         rows: 2,
         fill:	'column',
@@ -68,22 +95,29 @@ const swiper = new Swiper('.news__container', {
     },
     1440: {
       width: 1240,
-      slidesPerView: 3,
-      // slidesPerView: 'auto',
+      slidesPerView: 'auto',
+      slidesPerGroup: 3,
       spaceBetween: 32,
       simulateTouch: false,
     },
   },
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  //   draggable: true,
-  // },
-  // mousewheel: {
-  //   sensitivity: 1,
-  //   eventsTarget: '.news',
-  //   enabled: true,
-  //   eventsTarget: '.programs__pagination',
-  // }
+  on: {
+    resize: resizeSlides,
+    slideChangeTransitionStart: resizeSlides,
+    slideChangeTransitionEnd: resizeSlides,
+  },
 });
 
 swiper.init();
+swiper.on('slidesUpdated', resizeSlides);
+
+// scrollbar: {
+//   el: '.swiper-scrollbar',
+//   draggable: true,
+// },
+// mousewheel: {
+//   sensitivity: 1,
+//   eventsTarget: '.news',
+//   enabled: true,
+//   eventsTarget: '.programs__pagination',
+// }
