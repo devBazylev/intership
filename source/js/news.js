7ccc48937c34cd1b1273c9edae78ea984adce270
jar <dev.bazylev@gmail.com>
2024-06-14 14:47:08 +0300

    adds news pagination functionality

--- source/js/news.js
+++ source/js/news.js
@@ -21,60 +21,6 @@ cloneSlides(slider, slides, clones);
 cloneSlides(slider, slides, clones);
 cloneSlides(slider, slides, clones);
 
-const resizeSlides = () => {
-  const slidesWithClones = Array.from(news.querySelectorAll('.news__slide'));
-  setDataId(slidesWithClones);
-
-  if (mob.matches) {
-    slidesWithClones.forEach((slide) => {
-      const index = slidesWithClones.indexOf(slide);
-      const image = slide.querySelector('.news__image');
-      if (index % 2) {
-        slide.style.width = '290px';
-        slide.style.height = '240px';
-        image.style.height = slide.style.height;
-      } else {
-        slide.style.width = '290px';
-        slide.style.height = '330px';
-        image.style.height = slide.style.height;
-      }
-    });
-  }
-  if (tab.matches) {
-    slidesWithClones.forEach((slide) => {
-      const image = slide.querySelector('.news__image');
-      slide.style.width = '324px';
-      slide.style.height = '350px';
-      image.style.height = slide.style.height;
-    });
-  }
-  if (desk.matches) {
-    slidesWithClones.forEach((slide) => {
-      const index = slidesWithClones.indexOf(slide);
-      const image = slide.querySelector('.news__image');
-      const dataId = slide.getAttribute('data-id');
-      slide.style.order = dataId;
-      if (index % 3) {
-        slide.style.width = '286px';
-        slide.style.height = '400px';
-        image.style.height = slide.style.height;
-      } else {
-        slide.style.width = '604px';
-        slide.style.height = '400px';
-        image.style.height = slide.style.height;
-      }
-      while (slidesWithClones.length % 3) {
-        const random = getRandomInteger(0, slides.length);
-        const clone = news.querySelectorAll('.news__slide')[random].cloneNode(true);
-        // const clone = slides[random].cloneNode(true);
-        clone.setAttribute('aria-hidden', true);
-        slidesWithClones.push(clone);
-        slider.appendChild(clone);
-      }
-    });
-  }
-};
-
 const tabsMenu = new Swiper('.news__wrapper', {
   modules: [Mousewheel, FreeMode],
   init: false,
@@ -119,8 +65,8 @@ const swiper = new Swiper('.news__container', {
   observer: true,
   observeParents: true,
   observeSlideChildren: true,
-  slideFullyVisibleClass: 'news__slide--full',
   slideActiveClass: 'news__slide--active',
+  slideNextClass: 'news__slide--next',
   loopAddBlankSlides: false,
   loopAdditionalSlides: 0,
   resizeObserver: true,
@@ -148,6 +94,7 @@ const swiper = new Swiper('.news__container', {
       slidesPerView: 1,
       slidesPerGroup: 1,
       spaceBetween: 20,
+      slideVisibleClass: 'news__slide--full',
       grid: {
         rows: 2,
         fill:	'column',
@@ -158,6 +105,7 @@ const swiper = new Swiper('.news__container', {
       slidesPerView: 2,
       slidesPerGroup: 2,
       spaceBetween: 30,
+      slideVisibleClass: 'news__slide--full',
       grid: {
         rows: 2,
         fill:	'row',
@@ -169,19 +117,103 @@ const swiper = new Swiper('.news__container', {
       slidesPerGroup: 3,
       spaceBetween: 32,
       simulateTouch: false,
+      slideVisibleClass: 'news__slide--full',
       grid: {
         rows: 1,
         fill:	'column',
       },
     },
   },
-  on: {
-    resize: resizeSlides,
-  },
 });
 
 tabsMenu.init();
 swiper.init();
+
+const resizeSlides = () => {
+  const slidesWithClones = Array.from(news.querySelectorAll('.news__slide'));
+  setDataId(slidesWithClones);
+
+  if (mob.matches) {
+    slidesWithClones.forEach((slide) => {
+      const index = slidesWithClones.indexOf(slide);
+      const image = slide.querySelector('.news__image');
+      if (index % 2) {
+        slide.style.width = '290px';
+        slide.style.height = '240px';
+        image.style.height = slide.style.height;
+      } else {
+        slide.style.width = '290px';
+        slide.style.height = '330px';
+        image.style.height = slide.style.height;
+      }
+    });
+  }
+  if (tab.matches) {
+    slidesWithClones.forEach((slide) => {
+      const image = slide.querySelector('.news__image');
+      slide.style.width = '324px';
+      slide.style.height = '350px';
+      image.style.height = slide.style.height;
+    });
+  }
+  if (desk.matches) {
+    slidesWithClones.forEach((slide) => {
+      const index = slidesWithClones.indexOf(slide);
+      const image = slide.querySelector('.news__image');
+      if (index % 3) {
+        slide.style.width = '286px';
+        slide.style.height = '400px';
+        image.style.height = slide.style.height;
+      } else {
+        slide.style.width = '604px';
+        slide.style.height = '400px';
+        image.style.height = slide.style.height;
+      }
+      while (slidesWithClones.length % 3) {
+        const random = getRandomInteger(0, slides.length - 1);
+        const clone = slides[random].cloneNode(true);
+        clone.setAttribute('aria-hidden', true);
+        clones.push(clone);
+        slidesWithClones.push(clone);
+        slider.appendChild(clone);
+        setDataId(slidesWithClones);
+        // const dataId = clone.getAttribute('data-id');
+        // clone.style.order = dataId;
+      }
+    });
+  }
+};
+
+const renderPagination = () => {
+  const pagination = news.querySelector('.news__pagination');
+  const bullets = pagination.querySelectorAll('.news__bullet');
+  setDataId(bullets);
+  const activeBullet = pagination.querySelector('.news__bullet--active');
+  const activeBulletId = +activeBullet.getAttribute('data-id');
+  if (activeBulletId > 2 && activeBulletId < bullets.length - 1) {
+    const indexBeforeActive = activeBulletId - 3;
+    const indexAfterActive = activeBulletId + 1;
+    bullets[indexBeforeActive].classList.remove('news__bullet--active-main');
+    bullets[indexAfterActive].classList.add('news__bullet--active-main');
+  }
+  if (!activeBulletId < 1 && activeBulletId < bullets.length - 3) {
+    const indexBeforeActive = activeBulletId - 1;
+    const indexAfterActive = activeBulletId + 3;
+    bullets[indexBeforeActive].classList.add('news__bullet--active-main');
+    bullets[indexAfterActive].classList.remove('news__bullet--active-main');
+  }
+  if (mob.matches) {
+    pagination.style.width = '128px';
+  }
+  if (tab.matches) {
+    pagination.style.width = '160px';
+  }
+  if (desk.matches) {
+    pagination.style.width = '160px';
+  }
+};
+
 swiper.on('slidesUpdated', resizeSlides);
+swiper.on('paginationUpdate', renderPagination);
 
 addListenerArray(tabs, 'click', onTab);
